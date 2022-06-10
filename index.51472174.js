a1599da34157bbe3ea997f63b6719f9df7cd6484
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-06-10 09:45:23 +0000

    Deploying to gh-pages from @ Iura-Radulov/teamproject-filmoteka@75f611bbc08b7bc190a7aedb3cce95194a665068 🚀

---
+++ index.51472174.js
@@ -0,0 +1,2 @@
+fetch("https://api.themoviedb.org/3/trending/all/week?api_key=8c768371227c12dea02a7fda24385a83").then((e=>e.json())).then((e=>console.log(e)));
+//# sourceMappingURL=index.51472174.js.map
