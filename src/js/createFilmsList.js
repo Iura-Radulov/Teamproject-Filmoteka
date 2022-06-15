356f8d4c72d759d7fda258f6d2311d95c7ee60fd
MatveyRubets <80116702+MatveyRubets@users.noreply.github.com>
2022-06-15 10:15:33 +0200

    Ft 05,06 (#36)
    * FT-05,06
    
    * breakpoint fixed, img height fixed
    
    Co-authored-by: Iuri <91605430+Iura-Radulov@users.noreply.github.com>

--- src/js/createFilmsList.js
+++ src/js/createFilmsList.js
@@ -17,32 +17,25 @@ export default function createFilmsList(dates) {
           poster_path !== null
             ? `https://image.tmdb.org/t/p/w500${poster_path}`
             : `https://www.online-tech-tips.com/wp-content/uploads/2022/03/image-41.jpeg`
-        } alt="" loading="lazy" width="310" data-id=${id} />
+
+        } alt="" loading="lazy" width="310" height="449" data-id=${id} />
+
         <div class="info">
           <p class="film__info-name">${
             original_title ? original_title : original_name
           }
           </p>
 
-          <p class="info-item">
-            <b>${
-              genre_ids.length > 2
-                ? genreArray
-                    .reduce((listGenre, genre) => {
-                      if (genre_ids.includes(genre.id)) {
-                        listGenre.push(` ${genre.name}`);
-                      }
-                      return listGenre;
-                    }, [])
-                    .slice(0, 2)
-                    .concat([' Other'])
-                : genreArray.reduce((listGenre, genre) => {
-                    if (genre_ids.includes(genre.id)) {
-                      listGenre.push(` ${genre.name}`);
-                    }
-                    return listGenre;
-                  }, [])
-            } </b >
+          <p class="film__info-desc">
+            <b>${genreArray
+              .reduce((listGenre, genre) => {
+                if (genre_ids.includes(genre.id)) {
+                  listGenre.push(` ${genre.name}`);
+                }
+                return listGenre;
+              }, [])
+              .slice(0, 2)
+              .concat([' Other'])} </b >
 
             <b>|</b>
             <b>${release_date ? release_date.slice(0, 4) : '-'}</b>
