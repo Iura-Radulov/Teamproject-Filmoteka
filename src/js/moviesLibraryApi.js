56611924b8ec24a5970c79cb795cf5e26a321431
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-06-15 23:12:25 +0300

    change background of library (#44)
    Co-authored-by: Iuri <91605430+Iura-Radulov@users.noreply.github.com>

--- src/js/moviesLibraryApi.js
+++ src/js/moviesLibraryApi.js
@@ -11,21 +11,37 @@ const showHomeBtn = document.getElementById('home');
 const search = document.querySelector('.search-form');
 const container = document.querySelector('.films__container');
 const libraryButtons = document.querySelector('.buttons');
+const header = document.querySelector('.header');
 
 const WATCHED_MOVIES = 'watchedMovies/';
 const MOVIES_QUEUE = 'queueOfMovies/';
 const IS_HIDDEN = 'is-hidden';
 const CURRENT_LINK = 'current-link';
+const HEADER_BGR = 'header__background';
+const HEADER_BGR_LIBRARY = 'header__background-library';
 
 showWatchedBtn.addEventListener('click', onWatchedBtnClick);
 showQueueBtn.addEventListener('click', onQueueBtnClick);
 showLibraryBtn.addEventListener('click', onLibraryBtnClick);
+
+showHomeBtn.addEventListener('click', onHomeBtnClick);
+
+function onHomeBtnClick() {
+  libraryButtons.classList.add(IS_HIDDEN);
+  search.classList.remove(IS_HIDDEN);
+  header.classList.remove(HEADER_BGR_LIBRARY);
+  header.classList.add(HEADER_BGR);
+}
+
 
 async function onLibraryBtnClick() {
   showLibraryBtn.classList.add(CURRENT_LINK);
   showHomeBtn.classList.remove(CURRENT_LINK);
   libraryButtons.classList.remove(IS_HIDDEN);
   search.classList.add(IS_HIDDEN);
+  header.classList.remove(HEADER_BGR);
+  header.classList.add(HEADER_BGR_LIBRARY);
+
 
   const response = await fetchMoviesFromDatabase(WATCHED_MOVIES);
   showLibrary(response);
