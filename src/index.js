4a240838109b03ba798ba7cc97e737b93a00ee71
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-06-18 12:47:25 +0300

    close loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import loadData from './js/loader';
-loadData();
-import './js/loader';
 import './js/fetchdata';
 import './js/firebase/firebase';
 import './js/pagination';
 import './js/firebase/handleRegister';
 import './js/firebase/handleLogin';
 import './js/footerTeam';
+import './js/loader';
