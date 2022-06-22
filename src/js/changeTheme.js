337fdc35f9dbd56af99f176fcaec4be6b13e7dbb
Roman Havrylenko <g4vr0m@gmail.com>
2022-06-22 18:00:59 +0300

    localstorage lang+theme

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,27 +1,25 @@
 const changeBtn = document.getElementById('change-theme-btn');
 
-
-
 function setDarkTheme() {
-    document.body.classList.add('dark')
-    changeBtn.textContent = "🌞"
-   
+  document.body.classList.add('dark');
+  changeBtn.textContent = '🌞';
+  localStorage.setItem('theme', 'dark');
 }
 
 function setLightTheme() {
-    document.body.classList.remove('dark')
-    changeBtn.textContent = "🌙"    
-    localStorage.theme = 'light'
+  document.body.classList.remove('dark');
+  changeBtn.textContent = '🌙';
+  localStorage.setItem('theme', 'light');
 }
 
 changeBtn.addEventListener('click', () => {
-    if (document.body.classList.contains('dark')) {
-    setLightTheme()
-    } else {
-    setDarkTheme()
-    }
-})
+  if (document.body.classList.contains('dark')) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+});
 
-if (localStorage.theme === 'dark') {
-    setDarkTheme()
+if (localStorage.getItem('theme') === 'dark') {
+  setDarkTheme();
 }
